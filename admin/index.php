e09dbc72595ee194892899212f02356b4a59a74e
Hritik <hritikgl518@gmail.com>
2023-02-06 09:21:48 +0530

    first commit

---
+++ admin/index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html dir="ltr">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" type="image/png" sizes="16x16" href="../assets/images/favicon.png">
+    <title>Hostel Management System</title>
+    <link href="../dist/css/style.min.css" rel="stylesheet">
+
+</head>
+
+<body>
+    <div class="main-wrapper">
+        <div class="auth-wrapper d-flex no-block justify-content-center align-items-center position-relative"
+            style="background:url(../assets/images/big/auth-bg.jpg) no-repeat center center;">
+            <div class="auth-box row">
+                <div class="col-lg-7 col-md-5 modal-bg-img" style="background-image: url(../assets/images/adimg.jpg);">
+                </div>
+                <div class="col-lg-5 col-md-7 bg-white">
+                    <div class="p-3">
+                        <div class="text-center">
+                            <img src="../assets/images/big/icon.png" alt="wrapkit">
+                        </div>
+                        <h2 class="mt-3 text-center">Admin Login</h2>
+                        
+                        <form class="mt-4" method="POST">
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <div class="form-group">
+                                        <label class="text-dark" for="uname">Email or Username</label>
+                                        <input class="form-control" name="username" id="uname" type="text"
+                                            placeholder="Enter your Email" required>
+                                    </div>
+                                </div>
+                                <div class="col-lg-12">
+                                    <div class="form-group">
+                                        <label class="text-dark" for="pwd">Password</label>
+                                        <input class="form-control" name="password" id="pwd" type="password"
+                                            placeholder="Enter your password" required>
+                                    </div>
+                                </div>
+                                <div class="col-lg-12 text-center">
+                                    <button type="submit" name="login" class="btn btn-block btn-danger">LOGIN</button>
+                                </div>
+                                <div class="col-lg-12 text-center mt-5">
+                                   <a href="../index.php" class="text-danger">Go Back</a>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
