5f1ac8da4cba974451c605b04fc8d924c5432780
Hritik <hritikgl518@gmail.com>
2023-03-27 09:17:37 +0530

    added grievance.php

--- includes/student-sidebar.php
+++ includes/student-sidebar.php
@@ -22,6 +22,10 @@
         <li class="sidebar-item"> <a class="sidebar-link sidebar-link" href="log-activity.php"
         aria-expanded="false"><i class="fas fa-cogs"></i><span
         class="hide-menu">Log Activities</span></a></li>
+
+        <li class="sidebar-item"> <a class="sidebar-link sidebar-link" href="add_grievance.php"
+        aria-expanded="false"><i class="fas fa-cogs"></i><span
+        class="hide-menu">Add Grievance</span></a></li>
                            
     </ul>
 </nav>
