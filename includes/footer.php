22ac31ece2d13c15853561794702e6639daa8f55
Aayush Barmecha <barmecha99@gmail.com>
2023-04-02 18:33:12 +0530

    Almost Final Changes

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,3 @@
 <footer class="footer text-center text-muted">
-&copy; <?php echo date("Y"); ?> - Hostel Management System - Developed by Hritik Aayush Cyrus</a>
+&copy; <?php echo date("Y"); ?> - Hostel Management System - Developed by Hritik| Aayush| Cyrus</a>
 </footer>
